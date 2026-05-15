741f021782e722dc15a7beaa03a58e17d633db5f
Aryan Kumar Shrivastav <aryan_097@arramton.com>
2026-05-15 16:51:53 +0530

    adding web url

--- web/lib/config.ts
+++ web/lib/config.ts
@@ -1,7 +1,8 @@
 const config = {
-	// apiUrl: "http://localhost:8500/", <-- localhost
+	// apiUrl: "http://localhost:8500/", <-- local
 	apiUrl: "https://api-credaxis.onrender.com/", // live url 
-	webUrl: "http://localhost:3000",
+	// webUrl: "http://localhost:3000", <-- local
+	webUrl: "https://credaxis.vercel.app", // live url
 };
 
 
